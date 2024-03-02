ce34556d0bf7c22365deec3433a775b058d47aa4
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2024-03-02 11:19:14 +0530

    removed login for landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,11 +15,12 @@ function App() {
   return (
     <div>
       <Router>
-      {isAuthenticated && <Header />}
+      {/* {isAuthenticated && <Header />} */}
+      <Header />
         <Routes>
        
         <Route path="/products" element={<Page1 />} />
-        <Route path="/login" element={<LoginSignUp />} />
+        {/* <Route path="/login" element={<LoginSignUp />} /> */}
         <Route path="/Profile" element={<Profile />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/products/:keyword" element={<Page1 />} />
@@ -27,9 +28,9 @@ function App() {
     
         <Route path="/:id" element={<ProductDetails />} />
         <Route path="/main" element={<Page1 />} />
-        {/* <Route path="/" element={<Page1 />} /> */}
+        <Route path="/" element={<Page1 />} />
         
-        <Route path="/" element={isAuthenticated ? <Page1/>: <LoginSignUp />}/>
+        {/* <Route path="/" element={isAuthenticated ? <Page1/>: <LoginSignUp />}/> */}
         </Routes>
         
       </Router>
